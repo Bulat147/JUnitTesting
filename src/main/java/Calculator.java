9e4acd4280b47d7900d89806f18ad41cb2d995b9
ninza <ninza147258@gmail.com>
2022-07-29 09:42:50 +0300

    first step of testing Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int minus(int a, int b){
+        return a-b;
+    }
+
+    public double divide(double firstNum, double secondNum){
+        return firstNum/secondNum; // Здесь могут возникнуть погрешности, т.к. работаем с double
+    }
+
+    public double multiply(double firstNum, double secondNum){
+        return firstNum * secondNum; // Тоже могут возникнуть погрешности
+    }
+
+    public double degree(double a, int b){
+        double temp = a;
+        for (int i=0; i<b-1; i++){
+            a = temp*a; // Тоже с погрешностями
+        }
+        return a;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,75 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class CalculatorTest {
-  
+
+class CalculatorTest { // В обычном виде этот класс никак не связан с классом Calculator,
+            // свяжется он только когда мы создадим объект класса Calculator в каком-то из его методов
+    private Calculator calculator;
+
+    // Дескриптор для сетапа - BeforeEach
+    @BeforeEach
+    void setUp() { // Сделал это чисто по фану, хотя можно было и в поле класса теста инициализировать
+        calculator = new Calculator();
+    }
+
+    // Дескриптор для теста - Test
+    @Test
+    void add() {
+        // Этот метод никак не связан с методом add в классе Calculator,
+        // мы просто работаем в нем с объектом Calculator и ещё у этих методов название одинаковое и всё
+        int expected;
+        int result;
+        for (int i=1; i<50; i++){
+            expected = i + (i-1);
+            result = calculator.add(i, i-1);
+            assertEquals(expected, result);
+        }
+    }
+
+    @Test
+    void minus(){
+        int expected;
+        int result;
+        for (int i=1; i<50; i++){
+            expected = i - (i-1);
+            result = calculator.minus(i, i-1);
+            assertEquals(expected, result);
+        }
+    }
+
+    @Test
+    void multiply(){
+        double expected;
+        double result;
+        for (double i=1.5; i<50; i++){
+            expected = i * (i-1);
+            result = calculator.multiply(i, i-1);
+            // Т.к. при double могут быть погрешности, то сравнивать их нужно с учетом погрешности
+            assertEquals(expected, result, 0.000000001);
+        }
+    }
+
+    @Test
+    void divide(){
+        double expected;
+        double result;
+        for (double i=1.5; i<50; i++){
+            expected = i / (i-1);
+            result = calculator.divide(i, i-1);
+            // Т.к. при double могут быть погрешности, то сравнивать их нужно с учетом погрешности
+            assertEquals(expected, result, 0.000000001);
+        }
+    }
+
+    @Test
+    void degree(){
+        double expected = 25;
+        double result = calculator.degree(5, 2);
+        assertEquals(expected, result);
+
+        double expected2 = 216;
+        double result2 = calculator.degree(6, 3);
+        assertEquals(expected2, result2);
+    }
 }
